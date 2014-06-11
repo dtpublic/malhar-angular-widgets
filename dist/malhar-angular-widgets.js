6de95bc6076c444ff0ef1d025ae888629d41aa49
Nick Holub <nikholub@gmail.com>
2014-06-10 17:35:40 -0700

    wt-select directive

--- dist/malhar-angular-widgets.js
+++ dist/malhar-angular-widgets.js
@@ -1257,6 +1257,50 @@ angular.module('ui.widgets')
 'use strict';
 
 angular.module('ui.widgets')
+  .directive('wtSelect', function () {
+    return {
+      restrict: 'A',
+      replace: true,
+      templateUrl: 'template/widgets/select/select.html',
+      scope: {
+        label: '@',
+        value: '=',
+        options: '='
+      },
+      controller: function ($scope) {
+        $scope.prevValue = function () {
+          var index = $scope.options.indexOf($scope.value);
+          var nextIndex = (index - 1 + $scope.options.length) % $scope.options.length;
+          $scope.value = $scope.options[nextIndex];
+        };
+
+        $scope.nextValue = function () {
+          var index = $scope.options.indexOf($scope.value);
+          var nextIndex = (index + 1) % $scope.options.length;
+          $scope.value = $scope.options[nextIndex];
+        };
+      }
+    };
+  });
+/*
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+angular.module('ui.widgets')
   .directive('wtTime', function ($interval) {
     return {
       restrict: 'A',
@@ -1384,6 +1428,19 @@ angular.module("ui.widgets").run(["$templateCache", function($templateCache) {
     "<div>\n" +
     "    Value\n" +
     "    <div class=\"alert\" ng-class=\"valueClass || 'alert-warning'\">{{scopeValue || 'no data'}}</div>\n" +
+    "</div>"
+  );
+
+  $templateCache.put("template/widgets/select/select.html",
+    "<div>\n" +
+    "    {{label}} <select ng-model=\"value\" ng-options=\"opt for opt in options\"\n" +
+    "                          class=\"form-control\" style=\"width: 200px; display: inline;\"></select>\n" +
+    "    <button type=\"button\" class=\"btn btn-default\" ng-click=\"prevValue();\">\n" +
+    "        <span class=\"glyphicon glyphicon-chevron-left\"></span>\n" +
+    "    </button>\n" +
+    "    <button type=\"button\" class=\"btn btn-default\">\n" +
+    "        <span class=\"glyphicon glyphicon-chevron-right\" ng-click=\"nextValue();\"></span>\n" +
+    "    </button>\n" +
     "</div>"
   );
 
---
+++ src/widgets/select/select.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+angular.module('ui.widgets')
+  .directive('wtSelect', function () {
+    return {
+      restrict: 'A',
+      replace: true,
+      templateUrl: 'template/widgets/select/select.html',
+      scope: {
+        label: '@',
+        value: '=',
+        options: '='
+      },
+      controller: function ($scope) {
+        $scope.prevValue = function () {
+          var index = $scope.options.indexOf($scope.value);
+          var nextIndex = (index - 1 + $scope.options.length) % $scope.options.length;
+          $scope.value = $scope.options[nextIndex];
+        };
+
+        $scope.nextValue = function () {
+          var index = $scope.options.indexOf($scope.value);
+          var nextIndex = (index + 1) % $scope.options.length;
+          $scope.value = $scope.options[nextIndex];
+        };
+      }
+    };
+  });
